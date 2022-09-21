2b5fafa3950a285c15f526ca9044ec03ee06f2df
Mesi <mse212002@gmail.com>
2022-09-21 19:05:00 +0300

    :art: remove comments and add string interpolation where needed

--- index.rb
+++ index.rb
@@ -1,4 +1,4 @@
-CHARS_HASH = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D',
+CHARS_HASH = { '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D',
    '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I',
    '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M',
    '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q',
@@ -6,35 +6,31 @@ CHARS_HASH = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D',
    '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
    '--..': 'Z', '.----': '1', '..---': '2', '...--': '3',
    '....-': '4', '.....': '5', '-....': '6', '--...': '7',
-   '---..': '8', '----.': '9', '-----': '0' }
+   '---..': '8', '----.': '9', '-----': '0' }.freeze
 
 def decode_char(char)
-   return CHARS_HASH[:"#{char}"];
+CHARS_HASH[:"#{char}"]
 end
 
 def decode_word(word)
-   words = word.split(' ');
-   result = '';
-   words.each do |i|
-   result += decode_char(i)
-   end
-return result
+words = word.split(' ')
+result = ''
+words.each do |i|
+result += decode_char(i)
+end
+result
 end
 
 def decode(sentence)
-   sentences = sentence.split('  ');
-   result = '';
-   sentences.each do |i|
-   result += decode_word(i)+ " "
-   end
+sentences = sentence.split('  ')
+result = ''
+sentences.each do |i|
+result += "#{decode_word(i)} "
+end
 puts result
 end
 
-puts decode_char(".-")
-# A
+puts decode_char('.-')
 puts decode_word('-- -.--')
-# MY
-decode("-- -.--   -. .- -- .")
-# MY NAME
-decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
-# A BOX FULL OF RUBIES
+decode('-- -.--   -. .- -- .')
+decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
