b21d75542868c593233340b79b072ff348eb56da
Mesi <mse212002@gmail.com>
2022-09-21 19:06:01 +0300

    :rotating_light: fix rubocop errors

--- index.rb
+++ index.rb
@@ -1,33 +1,33 @@
 CHARS_HASH = { '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D',
-   '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I',
-   '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M',
-   '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q',
-   '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U',
-   '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
-   '--..': 'Z', '.----': '1', '..---': '2', '...--': '3',
-   '....-': '4', '.....': '5', '-....': '6', '--...': '7',
-   '---..': '8', '----.': '9', '-----': '0' }.freeze
+               '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I',
+               '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M',
+               '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q',
+               '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U',
+               '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
+               '--..': 'Z', '.----': '1', '..---': '2', '...--': '3',
+               '....-': '4', '.....': '5', '-....': '6', '--...': '7',
+               '---..': '8', '----.': '9', '-----': '0' }.freeze
 
 def decode_char(char)
-CHARS_HASH[:"#{char}"]
+  CHARS_HASH[:"#{char}"]
 end
 
 def decode_word(word)
-words = word.split(' ')
-result = ''
-words.each do |i|
-result += decode_char(i)
-end
-result
+  words = word.split(' ')
+  result = ''
+  words.each do |i|
+    result += decode_char(i)
+  end
+  result
 end
 
 def decode(sentence)
-sentences = sentence.split('  ')
-result = ''
-sentences.each do |i|
-result += "#{decode_word(i)} "
-end
-puts result
+  sentences = sentence.split('  ')
+  result = ''
+  sentences.each do |i|
+    result += "#{decode_word(i)} "
+  end
+  puts result
 end
 
 puts decode_char('.-')
